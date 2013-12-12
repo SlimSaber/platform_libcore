multi-os-engine/libcore

Improve URI.equals performance.
The main improvement is that we're vastly reducing
the amount of garbage this method creates.

Also, get rid of some incorrect checks and add a new
benchmark test case.

Before vs. After:

URI                            Equals     1540.1 =
URI EqualsWithHeavilyEscapedComponent 11921397.1 ==============================

URI                            Equals     1337.2 =
URI EqualsWithHeavilyEscapedComponent   588934.3 ==============================

Change-Id: I9ddb87b8267f0ac6a3ac85c2989cc0dd305ef6a1

luni/src/main/java/java/net/URI.java
@@ -766,33 +766,51 @@ public final class URI implements Comparable<URI>, Serializable {
     }
 
     /**
-     * Returns true if {@code first} and {@code second} are equal after
-     * unescaping hex sequences like %F1 and %2b.
+     * Returns true if the given URI escaped strings {@code first} and {@code second} are
+     * equal.
+     *
+     * TODO: This method assumes that both strings are escaped using the same escape rules
+     * yet it still performs case insensitive comparison of the escaped sequences.
+     * Why is this necessary ? We can just replace it with first.equals(second)
+     * otherwise.
      */
     private boolean escapedEquals(String first, String second) {
-        if (first.indexOf('%') != second.indexOf('%')) {
-            return first.equals(second);
+        // This length test isn't a micro-optimization. We need it because we sometimes
+        // calculate the number of characters to match based on the length of the second
+        // string. If the second string is shorter than the first, we might attempt to match
+        // 0 chars, and regionMatches is specified to return true in that case.
+        if (first.length() != second.length()) {
+            return false;
         }
 
         int index, prevIndex = 0;
-        while ((index = first.indexOf('%', prevIndex)) != -1
-                && second.indexOf('%', prevIndex) == index) {
-            boolean match = first.substring(prevIndex, index).equals(
-                    second.substring(prevIndex, index));
-            if (!match) {
+        while (true) {
+            int index1 = first.indexOf('%', prevIndex);
+            int index2 = second.indexOf('%', prevIndex);
+            if (index1 != index2) {
                 return false;
             }
 
-            match = first.substring(index + 1, index + 3).equalsIgnoreCase(
-                    second.substring(index + 1, index + 3));
-            if (!match) {
+            index = index1;
+            if (index == -1) {
+                // No more escapes, match the remainder of the string
+                // normally.
+                break;
+            }
+
+            if (!first.regionMatches(prevIndex, second, prevIndex, (index - prevIndex))) {
+                return false;
+            }
+
+            if (!first.regionMatches(true /* ignore case */, index + 1, second, index + 1, 2)) {
                 return false;
             }
 
             index += 3;
             prevIndex = index;
         }
-        return first.substring(prevIndex).equals(second.substring(prevIndex));
+
+        return first.regionMatches(prevIndex, second, prevIndex, second.length() - prevIndex);
     }
 
     @Override public boolean equals(Object o) {

SlimSaber/platform_libcore

luni/src/main/java/java/net/URI.java
@@ -766,33 +766,51 @@ public final class URI implements Comparable<URI>, Serializable {
     }
 
     /**
-     * Returns true if {@code first} and {@code second} are equal after
-     * unescaping hex sequences like %F1 and %2b.
+     * Returns true if the given URI escaped strings {@code first} and {@code second} are
+     * equal.
+     *
+     * TODO: This method assumes that both strings are escaped using the same escape rules
+     * yet it still performs case insensitive comparison of the escaped sequences.
+     * Why is this necessary ? We can just replace it with first.equals(second)
+     * otherwise.
      */
     private boolean escapedEquals(String first, String second) {
-        if (first.indexOf('%') != second.indexOf('%')) {
-            return first.equals(second);
+        // This length test isn't a micro-optimization. We need it because we sometimes
+        // calculate the number of characters to match based on the length of the second
+        // string. If the second string is shorter than the first, we might attempt to match
+        // 0 chars, and regionMatches is specified to return true in that case.
+        if (first.length() != second.length()) {
+            return false;
         }
 
-        int index, prevIndex = 0;
-        while ((index = first.indexOf('%', prevIndex)) != -1
-                && second.indexOf('%', prevIndex) == index) {
-            boolean match = first.substring(prevIndex, index).equals(
-                    second.substring(prevIndex, index));
-            if (!match) {
+        int prevIndex = 0;
+        while (true) {
+            int index = first.indexOf('%', prevIndex);
+            int index1 = second.indexOf('%', prevIndex);
+            if (index != index1) {
                 return false;
             }
 
-            match = first.substring(index + 1, index + 3).equalsIgnoreCase(
-                    second.substring(index + 1, index + 3));
-            if (!match) {
+            // index == index1 from this point on.
+
+            if (index == -1) {
+                // No more escapes, match the remainder of the string
+                // normally.
+               return first.regionMatches(prevIndex, second, prevIndex,
+                       second.length() - prevIndex);
+            }
+
+            if (!first.regionMatches(prevIndex, second, prevIndex, (index - prevIndex))) {
+                return false;
+            }
+
+            if (!first.regionMatches(true /* ignore case */, index + 1, second, index + 1, 2)) {
                 return false;
             }
 
             index += 3;
             prevIndex = index;
         }
-        return first.substring(prevIndex).equals(second.substring(prevIndex));
     }
 
     @Override public boolean equals(Object o) {