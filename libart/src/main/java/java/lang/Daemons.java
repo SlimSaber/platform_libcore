454326ac272ce6bc8aa3dae808063560c340cae8
Mathieu Chartier <mathieuc@google.com>
2014-10-22 17:31:12 -0700

    Only allow once GC request at a time.
    There is no reason to allow multiple, allocating threads may have
    multiple GC requests before the GC starts.
    
    Bug: 17942071
    
    (cherry picked from commit 71891f9da2fbcb865931da70ae759032e6698106)
    
    Change-Id: I5e2162373bcef1027eb71e5b1699186ff015f92d

--- libart/src/main/java/java/lang/Daemons.java
+++ libart/src/main/java/java/lang/Daemons.java
@@ -312,11 +312,9 @@ public final class Daemons {
 
     private static class GCDaemon extends Daemon {
         private static final GCDaemon INSTANCE = new GCDaemon();
-        private int count = 0;
 
         public void requestGC() {
             synchronized (this) {
-                ++count;
                 notify();
             }
         }
@@ -325,11 +323,8 @@ public final class Daemons {
             while (isRunning()) {
                 try {
                     synchronized (this) {
-                        // Wait until a request comes in, unless we have a pending request.
-                        while (count == 0) {
-                            wait();
-                        }
-                        --count;
+                        // Wait until a request comes in.
+                        wait();
                     }
                     VMRuntime.getRuntime().concurrentGC();
                 } catch (InterruptedException ignored) {
